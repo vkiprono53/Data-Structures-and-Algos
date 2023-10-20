7b76ff137096e75bb85e45e20143366fd7fa0486
Kiprono <kipronovincent53@gmail.com>
2023-10-20 18:39:01 +0300

    contains duplicate

---
+++ dsa/neetcode/neetcode/src/dsa/ContainsDuplicate.java
@@ -0,0 +1,41 @@
+package dsa;
+import java.util.Set;
+import java.util.HashSet;
+/**
+ *
+ * LEETCODE 217 : Contains Duplicate
+ *
+ *
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ *
+ * Example 2:
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ */
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+        int[] nums = {2,5,2,3,4};
+        System.out.println(containsDuplicate(nums));
+
+    }
+    /**
+     * Solution 1
+     * Time complexity - O(n)
+     * Space complexity - O(n)
+     */
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num: nums){
+            set.add(num);
+        }
+
+        return nums.length != set.size();
+    }
+}
