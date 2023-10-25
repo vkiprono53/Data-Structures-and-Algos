5629a8c92a860bba5a3accfbffe4a7448424dc62
Kiprono <kipronovincent53@gmail.com>
2023-10-25 09:32:35 +0300

    group anagrams and top k elements

---
+++ dsa/neetcode/src/dsa/GroupAnagrams.java
@@ -0,0 +1,53 @@
+package dsa;
+
+import java.util.*;
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * <p>
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * <p>
+ * Example 1:
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ */
+public class GroupAnagrams {
+    public static void main(String[] args) {
+
+        String word = "eat";
+        char[] charArr = word.toCharArray();
+        Arrays.sort(charArr);
+        System.out.println(new String(charArr));
+        String[] strsArrays = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+
+        System.out.println(groupAnagrams(strsArrays));
+    }
+
+    /**
+     *
+     * Solution 1 :
+     * Time Complexity - O(nklogk)
+     * Space complexity - O(nk)
+     * @return
+     */
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+        //Loop through the array:
+        for (String word : strs) {
+            char[] charString = word.toCharArray();
+            Arrays.sort(charString);
+            String outputString = new String(charString);
+            if (map.containsKey(outputString)) {
+                map.get(outputString).add(word);
+            } else {
+                //List<String> myList = new ArrayList<>();
+              //  myList.add(word);
+                map.put(outputString, new ArrayList<>());
+            }
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
---
+++ dsa/neetcode/src/dsa/TopKFrequentElements.java
@@ -0,0 +1,55 @@
+package dsa;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * LEETCODE 347. Top K Frequent Elements
+ *
+ * Given an integer array nums and an integer k, return the k most frequent elements.
+ * You may return the answer in any order.
+ * Example 1:
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ */
+public class TopKFrequentElements {
+    public static void main(String[] args) {
+
+        int[] nums = {1,1,1,2,2,3};
+        int  k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums, k)));
+    }
+
+    /**
+     * Time Complexity: O(n log(k)), where n is the number of elements in nums.
+     * Space Complexity: O(n + k), for the counts hash map with n elements and the heap of size k
+     * 
+     */
+    public static int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[k];
+
+        PriorityQueue<Map.Entry<Integer, Integer>> myQueue =
+                new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        //add map to the queue
+        myQueue.addAll(map.entrySet());
+
+       for (int i = 0; i <k ; i++){
+           result[i] = myQueue.poll().getKey();
+       }
+
+        return result;
+    }
+}
