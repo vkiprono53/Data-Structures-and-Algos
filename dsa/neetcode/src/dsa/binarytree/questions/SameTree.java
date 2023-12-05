455b4f13e6f1a384e34952ee1f3f65b4d5b6b17f
Vkiprono53 <kipronovincent53@gmail.com>
2023-12-05 19:33:09 +0300

    same tree

--- dsa/neetcode/src/dsa/binarytree/questions/SameTree.java
+++ dsa/neetcode/src/dsa/binarytree/questions/SameTree.java
@@ -14,7 +14,7 @@ import java.util.LinkedList;
  * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
  * Two binary trees are considered the same if they are structurally identical,
  * and the nodes have the same value.
- * 
+ *
  */
 
 public class SameTree {
