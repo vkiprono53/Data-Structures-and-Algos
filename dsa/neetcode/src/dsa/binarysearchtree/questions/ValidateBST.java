3cce2351cdc01aee48f07c2007fc908d51e7a8f2
Vkiprono53 <kipronovincent53@gmail.com>
2023-12-07 09:59:30 +0300

    Validate BST

---
+++ dsa/neetcode/src/dsa/binarysearchtree/questions/ValidateBST.java
@@ -0,0 +1,83 @@
+package dsa.binarysearchtree.questions;
+
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * @author vkiprono
+ * @created 12/7/23
+ */
+
+import dsa.binarysearchtree.BSTNode;
+import dsa.binarytree.TreeNode;
+
+/**
+ * LEETCODE 98. Validate Binary Search Tree
+ * <p>
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ * <p>
+ * A valid BST is defined as follows:
+ * <p>
+ * The left
+ * subtree
+ * of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ */
+public class ValidateBST {
+    List<Integer> myList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Time Complexity - O(n)
+     * Space Complexity - O(n)
+     * Using List
+     */
+    public boolean isValidBST(TreeNode root) {
+        myList = inOrderTraversal(root);
+
+        for (int i = 0; i < myList.size() - 1; i++) {
+            if (myList.get(i) >= myList.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> inOrderTraversal(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        inOrderTraversal(root.left);
+        myList.add(root.val);
+        inOrderTraversal(root.right);
+        return myList;
+    }
+
+    /**
+     * Recursion - No List
+     * Space - O(n)
+     * Time - O(n)
+     */
+
+    public boolean isValidBST1(TreeNode root){
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode root, Integer max, Integer min){
+
+        if (root == null){
+            return true;
+        }
+        else if(max != null && root.val >= max || min != null && root.val <= min ){
+            return false;
+        }
+        else {
+            return validate(root.left, root.val, min) && validate(root.right, max, root.val);
+        }
+    }
+
+}
--- dsa/neetcode/src/dsa/binarytree/questions/SubtreeAnotherTree.java
+++ dsa/neetcode/src/dsa/binarytree/questions/SubtreeAnotherTree.java
@@ -12,7 +12,7 @@ import dsa.binarytree.TreeNode;
  * Given the roots of two binary trees root and subRoot, return true if there is a subtree of
  * root with the same structure
  * and node values of subRoot and false otherwise.
- *
+ * <p>
  * A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's
  * descendants. The tree tree could also be considered as a subtree of itself.
  */
@@ -25,7 +25,7 @@ public class SubtreeAnotherTree {
         root.left.left = new TreeNode(11);
         root.left.right = new TreeNode(10);
 
-        TreeNode  subRoot = new TreeNode(2);
+        TreeNode subRoot = new TreeNode(2);
         subRoot.left = new TreeNode(11);
         subRoot.right = new TreeNode(10);
 
@@ -34,15 +34,18 @@ public class SubtreeAnotherTree {
 
     }
 
+
+    // Time Complexity: O(n)
+     // Space Complexity: O(n)
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
 
-        if ( subRoot == null){
+        if (subRoot == null) {
             return true;
         }
-        if (root == null){
+        if (root == null) {
             return false;
         }
-        if (isIdentical(root, subRoot)){
+        if (isIdentical(root, subRoot)) {
             return true;
         }
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
