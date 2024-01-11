962cfbe47a8864f3bd0ef02d9c93e3bb4d51b450
Vkiprono53 <kipronovincent53@gmail.com>
2024-01-11 08:29:51 +0300

    contains duplicate

--- dsa/neetcode/src/dsa/arraysandhashing/ContainsDuplicate.java
+++ dsa/neetcode/src/dsa/arraysandhashing/ContainsDuplicate.java
@@ -29,13 +29,17 @@ public class ContainsDuplicate {
      * Time complexity - O(n)
      * Space complexity - O(n)
      */
-    public static boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+    public static boolean containsDuplicate(int[] nums){
+        Set<Integer> mySet = new HashSet<>();
 
-        for (int num: nums){
-            set.add(num);
+        for (int num : nums){
+            if (mySet.contains(num)){
+                return true;
+            }
+            else{
+                mySet.add(num);
+            }
         }
-
-        return nums.length != set.size();
+        return false;
     }
 }
