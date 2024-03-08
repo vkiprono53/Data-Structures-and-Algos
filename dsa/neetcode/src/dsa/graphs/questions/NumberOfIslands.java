99f4f735893fa2997f1b27e4c0121f7e896b1692
Vkiprono53 <kipronovincent53@gmail.com>
2024-03-08 08:43:42 +0300

    number of islands

---
+++ dsa/neetcode/src/dsa/graphs/questions/NumberOfIslands.java
@@ -0,0 +1,55 @@
+package dsa.graphs.questions;
+
+/**
+ * @author vkiprono
+ * @created 3/8/24
+ */
+
+/**
+ * LEETCODE   200. Number of Islands
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water),
+ * return the number of islands.
+ * <p>
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+ */
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        NumberOfIslands islands = new NumberOfIslands();
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        System.out.println(islands.numIslands(grid));
+    }
+
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+
+        char[][] temp = grid;
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[i].length; j++) {
+                if (temp[i][j] == '1') {
+                    islands++;
+                    dfs(temp, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        //base case
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '2' || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '2';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+
+    }
+}
