a49785ad95c1a64b5e5abd47d21e089d6d5da9be
Vkiprono53 <kipronovincent53@gmail.com>
2024-03-20 07:45:19 +0300

    house robber 1

--- dsa/neetcode/src/dsa/dynamicprogramming/HouseRobber1.java
+++ dsa/neetcode/src/dsa/dynamicprogramming/HouseRobber1.java
@@ -38,8 +38,9 @@ public class HouseRobber1 {
 
         int[] dp = new int[len];
 
-        dp[0] = 0;
-        dp[1] = nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1], nums[0]);
+
 
         for (int i = 2; i < len; i++) {
             dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
