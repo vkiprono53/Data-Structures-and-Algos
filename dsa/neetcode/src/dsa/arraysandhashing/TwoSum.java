9baf2f8b6a65173ca9eb7afc11525d36de2b1770
vkiprono53 <kipronovincent53@gmail.com>
2024-03-28 09:16:23 +0300

    two sum brute force

--- dsa/neetcode/src/dsa/arraysandhashing/TwoSum.java
+++ dsa/neetcode/src/dsa/arraysandhashing/TwoSum.java
@@ -46,4 +46,20 @@ public class TwoSum {
         return null;
     }
 
+    /**
+     *Space Complexity - O(1)
+     * Time Complexity - O(n)
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+
+    }
+
 }
