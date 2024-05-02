c7457177bca8ca4b19e47e86b55d07877b0a5dc2
vkiprono53 <kipronovincent53@gmail.com>
2024-05-02 10:11:28 +0300

    340. Longest Substring with At Most K Distinct Characters

--- dsa/neetcode/src/dsa/slidingwindow/KDistinctChars.java
+++ dsa/neetcode/src/dsa/slidingwindow/KDistinctChars.java
@@ -5,6 +5,22 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ * Given a string s and an integer k, return the length of the longest substring of s that contains at
+ * most k distinct characters.
+ *
+ * Example 1:
+ *
+ * Input: s = "eceba", k = 2
+ * Output: 3
+ * Explanation: The substring is "ece" with length 3.
+ *
+ * Example 2:
+ *
+ * Input: s = "aa", k = 1
+ * Output: 2
+ * Explanation: The substring is "aa" with length 2.
+ */
 public class KDistinctChars {
     public static void main(String[] args) {
         String str = "aabbcc";
@@ -12,6 +28,11 @@ public class KDistinctChars {
         KDistinctChars distinctChars = new KDistinctChars();
         System.out.println(distinctChars.kDistinctChars(str, k));
     }
+
+    /**
+     * Time Complexity - O(n)
+     * Space Complexity - O(1)
+     */
     public int kDistinctChars(String str, int k) {
         int left = 0;
         int maxLen = 0;
@@ -23,7 +44,7 @@ public class KDistinctChars {
             char rightChar = str.charAt(right);
             charMap.put(rightChar, charMap.getOrDefault(rightChar, 0) + 1);
 
-            while (charMap.size() > k) {
+            if (charMap.size() > k) { // Or  while (charMap.size() > k) {
 
                 char charLeft = str.charAt(left);
                 if (charMap.containsKey(charLeft)) {
