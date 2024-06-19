43b512fb369ad41f68dbdd58341b76992fbc245b
vkiprono53 <kipronovincent53@gmail.com>
2024-06-19 16:05:05 +0300

    322. Coin Change

--- dsa/neetcode/src/dsa/dynamicprogramming/CoinChange.java
+++ dsa/neetcode/src/dsa/dynamicprogramming/CoinChange.java
@@ -40,7 +40,26 @@ public class CoinChange {
 
     }
 
+    /**
+     * Space Complexity - O(n)
+     * Time COmplexity = O(n * m) --n - amount, m - coin
+     */
     public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = amount + 1;
+        }
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return dp[amount] <= amount ? dp[amount] : -1;
+    }
+
+    public int coinChange1(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
 
         //Fill the array with something invalid
